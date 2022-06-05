be1a29ffa052aeecb183e62b23874c88ada29ade
[Nico242001] <nthiv077@uottawa.ca>
2022-06-05 17:58:46 -0400

    js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function service(service){
     window.location.replace("https://nico242001.github.io/reservation");
-
+    var elem = document.getElementById("service-reservation");
+    elem.value = "brushing";
 }
