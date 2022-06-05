0b2007cfc3f5d9c1e809fd306d2bcb3bc5fd1f7e
[Nico242001] <nthiv077@uottawa.ca>
2022-06-05 18:18:12 -0400

    m

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ function service(service){
     window.location.replace("https://nico242001.github.io/reservation");
     var elem = document.getElementById("service-reservation");
     elem.value = "brushing";
-    window.location.replace("https://nico242001.github.io/experts");
+    // window.location.replace("https://nico242001.github.io/experts");
 }
