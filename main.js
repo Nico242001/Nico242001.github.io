f3544ea5e0216a9b8eccbfc9f9a28a43c3a73f82
[Nico242001] <nthiv077@uottawa.ca>
2022-06-05 22:16:43 -0400

    params

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 function service(service){
-    var url = "https://nico242001.github.io/reservation?";
+    var url = "https://nico242001.github.io/reservation?service=";
+    var url = url.concat(service);
+    console.log(url);
+    window.location.replace(url);
+}
+
+function expert(expert){
+    var url = "https://nico242001.github.io/reservation?expert=";
     var url = url.concat(service);
     console.log(url);
     window.location.replace(url);
 }
 
 function reservation(){
-    var elem = document.getElementById("service-reservation");
-    elem.value = "brushing";
+    if(URLSearchParams.has("service")){
+        console.log("service");
+    }
+    if(URLSearchParams.has("expert")){
+        console.log("expert");
+    }
 }
+
+// var elem = document.getElementById("service-reservation");
+// elem.value = "brushing";
