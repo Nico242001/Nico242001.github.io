42a17f393c97da3cc49b79904d18dddc29a5ca94
[Nico242001] <nthiv077@uottawa.ca>
2022-06-05 21:30:34 -0400

    test

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ function service(service){
     elem.value = "brushing";
     // window.location.replace("https://nico242001.github.io/experts");
 }
+
+function test(){
+    var elem = document.getElementById("service-reservation");
+    elem.value = "brushing";
+}
