d22fbb6f6d4dabb83dae5f40db284a1cea22ff6d
[Nico242001] <nthiv077@uottawa.ca>
2022-06-28 00:13:33 -0400

    links

--- projet2/reviewItem.js
+++ projet2/reviewItem.js
@@ -1,18 +1,21 @@
 var sport;
 var equipement;
+var itemName
 var link = "https://nico242001.github.io/projet2/"
 
 function onload() {
     const params = new URLSearchParams(window.location.search);
     sport = params.get("sport");
     equipement = params.get("equipement");
+    itemName = params.get("itemName");
     if(equipement == "shinGuards"){
         equipement = "shin guards"
     }
-    var result = sport.concat(" ", equipement);
-    document.getElementById("sportNameEquipement").innerHTML = result;
+    var equipementLink = link.concat("items/?sport=",sport,"&equipement=",equipement);
     document.getElementById("sportLink").innerHTML = sport;
     document.getElementById("sportLink").href = link.concat(sport);
     document.getElementById("equipementLink").innerHTML = equipement;
-    document.getElementById("equipementLink").href = window.location;
+    document.getElementById("equipementLink").href = equipementLink;
+    document.getElementById("itemLink").innerHTML = itemName;
+    document.getElementById("itemLink").href = window.location;
 }
