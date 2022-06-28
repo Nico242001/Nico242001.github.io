56271ba4088701b3fe1a7a55a2fa49b454f333fc
[Nico242001] <nthiv077@uottawa.ca>
2022-06-28 12:43:06 -0400

    soccer filters

--- projet2/items.js
+++ projet2/items.js
@@ -6,6 +6,7 @@ function onload() {
     const params = new URLSearchParams(window.location.search);
     sport = params.get("sport");
     equipement = params.get("equipement");
+    filters();
     if(equipement == "shinGuards"){
         equipement = "shin guards"
     }
@@ -15,12 +16,21 @@ function onload() {
     document.getElementById("sportLink").href = link.concat(sport);
     document.getElementById("equipementLink").innerHTML = equipement;
     document.getElementById("equipementLink").href = window.location;
-    filters()
+    
 }
 
 function filters(){
     if(sport == "soccer"){
-        document.getElementById("filterContainer").innerHTML += "<h4>Brand</h4><input type='checkbox' id='vehicle1' name='vehicle1' value='Bike'><label for='vehicle1'> I have a bike</label><br><input type='checkbox' id='vehicle2' name='vehicle2' value='Car'><label for='vehicle2'> I have a car</label><br>"
+        document.getElementById("filterContainer").innerHTML += "<h4>Brand</h4><input type='checkbox' id='nike' name='nike' value='nike'><label for='nike'> Nike</label><br><input type='checkbox' id='adidas' name='adidas' value='adidas'><label for='adidas'> Adidas</label><br><input type='checkbox' id='puma' name='puma' value='puma'><label for='puma'> Puma</label><br>"
+        if(equipement == "cleats"){
+            document.getElementById("filterContainer").innerHTML += "<h4>Size</h4><input type='checkbox' id='6' name='6' value='6'><label for='6'> 6</label><br><input type='checkbox' id='7' name='7' value='7'><label for='7'> 7</label><br><input type='checkbox' id='8' name='8' value='8'><label for='8'> 8</label><br><input type='checkbox' id='9' name='9' value='9'><label for='9'> 9</label><br><input type='checkbox' id='10' name='10' value='10'><label for='10'> 10</label><br><input type='checkbox' id='11' name='11' value='11'><label for='11'> 11</label><br><input type='checkbox' id='12' name='12' value='12'><label for='12'> 12</label><br>"
+        }
+        if(equipement == "balls"){
+            document.getElementById("filterContainer").innerHTML += "<h4>Size</h4><input type='checkbox' id='1' name='1' value='1'><label for='1'> 1</label><br><input type='checkbox' id='2' name='2' value='2'><label for='2'> 2</label><br><input type='checkbox' id='3' name='3' value='3'><label for='3'> 3</label><br><input type='checkbox' id='4' name='4' value='4'><label for='4'> 4</label><br><input type='checkbox' id='5' name='5' value='5'><label for='5'> 5</label><br>"
+        }
+        if(equipement == "shinGuards"){
+            document.getElementById("filterContainer").innerHTML += "<h4>Size</h4><input type='checkbox' id='S' name='S' value='S'><label for='S'> Small</label><br><input type='checkbox' id='M' name='M' value='M'><label for='M'> Medium</label><br><input type='checkbox' id='L' name='L' value='L'><label for='L'> Large</label>"
+        }        
     }
     if(sport == "hockey"){
 
