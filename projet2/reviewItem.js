4a4ecea009f50a0157087063099db66ee54201e2
[Nico242001] <nthiv077@uottawa.ca>
2022-06-28 15:09:45 -0400

    progress

--- projet2/reviewItem.js
+++ projet2/reviewItem.js
@@ -3,16 +3,19 @@ var equipement;
 var itemID;
 var item = "Item"
 var link = "https://nico242001.github.io/projet2/"
+var imageLink;
 
 function onload() {
     const params = new URLSearchParams(window.location.search);
     sport = params.get("sport");
     equipement = params.get("equipement");
     itemID = params.get("itemName");
+    imageLink = params.get("imageLink");
     if(equipement == "shinGuards"){
         equipement = "shin guards"
     }
     var equipementLink = link.concat("items?sport=",sport,"&equipement=",equipement);
+    document.getElementById("image").src = imageLink;
     document.getElementById("sportLink").innerHTML = sport;
     document.getElementById("sportLink").href = link.concat(sport);
     document.getElementById("equipementLink").innerHTML = equipement;
